a7732079eb60ac05bd98d58e0394f951765fda3a
Deziane Sabino <deziane.sabino.santos@gmail.com>
2023-11-08 21:41:33 -0300

    Alteração do banco de dados para mysql

--- Palmirinha/settings.py
+++ Palmirinha/settings.py
@@ -77,10 +77,22 @@ WSGI_APPLICATION = 'Palmirinha.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'palmirinha_deziane',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   
+        'PORT': '3306',
     }
 }
 
